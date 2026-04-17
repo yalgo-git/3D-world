9331087fd1eb65ea25cb2f592eba3b051b356839
yalgo-git <senseitdd@gmail.com>
2026-04-17 15:02:21 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from direct.showbase.ShowBase import ShowBase
+
+class Game(ShowBase):
+    def __init__(self):
+        super().__init__()
+
+        self.model = self.loader.loadModel('models/environment')
+        self.model.reparentTo(self.render)
+
+        self.model.setScale(0.5)
+        self.model.setPos(0, 20, 0)
+        self.camLens.setFov(75)
+
+game = Game()
+game.run()
