86289b747848c45ad0f37d6ee8454c31d17b8a54
yalgo-git <senseitdd@gmail.com>
2026-04-24 15:09:47 +0000

    HII

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from direct.showbase.ShowBase import ShowBase
-
+from mapmanager import Mapmanager
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        self.land = Mapmanager()
+        base.camLens.setFov(90)
 
 game = Game()
 game.run()
